d920d66088694b00f7e3dec358741c70b90b60aa
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-09-30 23:37:41 +0500

    MongoDb post schema

---
+++ src/models/post.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 100,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Post = new mongoose.model("post", PostSchema);
+
+export default Post;
